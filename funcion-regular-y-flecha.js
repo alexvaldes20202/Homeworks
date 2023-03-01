e96079c438d096c8021bc3d16dee2017637578d6
alexvaldes20202 <50607581+alexvaldes20202@users.noreply.github.com>
2023-03-01 07:48:24 +0000

    Añado funcion flecha y regular

---
+++ funcion-regular-y-flecha.js
@@ -0,0 +1,15 @@
+function parOImpar(numero) {
+    if (numero % 2 === 0) {
+      console.log(numero + " es un número par.");
+    } else {
+      console.log(numero + " es un número impar.");
+    }
+  }
+
+  const parOImpar = (numero) => {
+    if (numero % 2 === 0) {
+      console.log(numero + " es un número par.");
+    } else {
+      console.log(numero + " es un número impar.");
+    }
+  };

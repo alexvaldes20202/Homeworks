63e5637cca4f8695b7309630424865cb0a93b7ac
alexvaldes20202 <50607581+alexvaldes20202@users.noreply.github.com>
2023-04-20 15:50:49 -0500

    Add files via upload

---
+++ TodoApp.jsx
@@ -0,0 +1,40 @@
+import React, { useReducer } from "react";
+import { TodoList } from "./TodoList";
+import { TodoAdd } from "./TodoAdd";
+import { TodoReducer } from "./TodoReducer";
+
+const initialState = [
+  {
+    id: new Date().getTime(),
+    description: "Hacer los challenges",
+    done: false,
+  },
+];
+
+export const TodoApp = () => {
+  const [todos, dispatch] = useReducer(TodoReducer, initialState);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: "[TODO] ADD TODO",
+      payload: todo,
+    };
+    dispatch(action);
+  };
+
+  return (
+    <>
+      <h1>TodoApp: 10,</h1>
+      <small>pendientes:2</small>
+      <hr />
+      <div className="row">
+        <div className="col-7">
+          <TodoList todos={todos} />
+        </div>
+        <div className="col-5">
+          <TodoForm onNewTodo={handleNewTodo} />
+        </div>
+      </div>
+    </>
+  );
+};
